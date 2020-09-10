5d606a9f468c8d7a4f66c9c36d8e3aa784f75f88
AfeezGL <afeezlg@gmail.com>
2020-09-10 13:35:32 -0700

    edits

--- product/models.py
+++ product/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator 
-from autoslug import AutoSlugField
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 from account.models import *
 
